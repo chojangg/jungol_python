be8e5ef8d5f96b9ef66145839eeb70232856f878
JAYUN <83991079+chojangg@users.noreply.github.com>
2022-07-06 23:49:09 +0900

    자가진단6

---
+++ 문자열1/문자열1_6.py
@@ -0,0 +1,3 @@
+print("%10s%10s%10s%10s" % ('item', 'count','price', 'rate'))
+print("%10s%10s%10s%10s" % ('item', 'count','price', 'rate'))
+print("%10s%10s%10s%10s" % ('item', 'count','price', 'rate'))
