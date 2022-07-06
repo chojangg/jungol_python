7caa37ca939ac6cc43457d7a7c17391ce8f73966
JAYUN <83991079+chojangg@users.noreply.github.com>
2022-07-06 23:48:30 +0900

    자가진단4

---
+++ 문자열1/문자열1_4.py
@@ -0,0 +1,7 @@
+ani1, sound1 = input().split()
+ani2, sound2 = input().split()
+ani3, sound3 = input().split()
+
+print(ani1,'sounds',sound1+'.')
+print(ani2,'sounds',sound2+'.')
+print(ani3,'sounds',sound3+'.')
