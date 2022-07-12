7c1964d0cf5f197fb6e6b4d54dfcf74d0b582e4e
JAYUN <83991079+chojangg@users.noreply.github.com>
2022-07-12 13:53:50 +0900

    자가진단 7-1, 7-2

---
+++ 선택제어문/선택제어문7-1.py
@@ -0,0 +1,14 @@
+grade = input()
+
+if grade == 'A':
+    print('Excellent')
+elif grade == 'B':
+    print('Good')
+elif grade == 'C':
+    print('Usually')
+elif grade == 'D':
+    print('Effort')
+elif grade == 'F':
+    print('Failure')
+else:
+    print('Mistake')
---
+++ 선택제어문/선택제어문7-2.py
@@ -0,0 +1,10 @@
+grade = float(input())
+
+if grade >= 4:
+    print('scholarship')
+elif grade >= 3:
+    print('next semester')
+elif grade >= 2:
+    print('seasonal semester')
+else:
+    print('retake')
