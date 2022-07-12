e72734c9724c332af0b87a75e2bd9ca6eed6b461
JAYUN <83991079+chojangg@users.noreply.github.com>
2022-07-12 13:53:32 +0900

    자가진단6

---
+++ 선택제어문/선택제어문6.py
@@ -0,0 +1,12 @@
+gender, age = input().split()
+age = int(age)
+
+if gender == 'M':
+    if age >= 18:
+        print('MAN')
+    else:
+        print('BOY')
+elif age >= 18:
+    print('WOMAN')
+else:
+    print('GIRL')
