4032be1f9cab4a22de960a4e3cd0052cd1a1536b
JAYUN <83991079+chojangg@users.noreply.github.com>
2022-08-29 23:17:50 +0900

    반복제어문3

---
+++ 반복제어문1/반복제어문3.py
@@ -0,0 +1,8 @@
+while True:
+    num = int(input("number? "))
+    if num > 0:
+        print("positive integer")
+    elif num < 0:
+        print("negative number")
+    if num == 0:
+        break
